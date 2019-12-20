78c941546bc12fbe023cb832c830fccba55a1e57
ravi <flavio.ravier@gmail.com>
2019-12-20 20:41:17 -0300

    So uns pequenos detalhes, mas é que eu esqueci de criar brand, então já vou colocar eles na master.

--- elo4-app/src/App.js
+++ elo4-app/src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+import ComponentLogin from './Components/ComponenteLogin/Index'
 
-import './App.css';
+
 
 function App() {
   return (
