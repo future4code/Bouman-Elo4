260aadd34c9257d9e9ab56b064e7d2bdb27e287a
eloisa-castro <elocastro31@gmail.com>
2019-12-21 10:25:08 -0300

    Inclusão do link para paleta de cores e imagem de fundo para o componente Lista

--- elo4-app/src/App.js
+++ elo4-app/src/App.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import axios from 'axios'
 import ComponentLogin from './Components/ComponenteLogin/Index'
 
-
+/* paleta de cores: https://coolors.co/f29711-5ce1e6-2e86ab-a23b72-c73e1d */
 
 function App() {
   return (
